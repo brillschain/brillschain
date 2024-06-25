8ca7b45a5147f799df462fc31cca41c565af90a1
K T Narasimha Naidu <141550407+naidu199@users.noreply.github.com>
2024-06-25 12:35:09 +0530

    Update custom_button.dart

--- lib/Home/widgets/custom_button.dart
+++ lib/Home/widgets/custom_button.dart
@@ -5,38 +5,51 @@ class CustomButton extends StatelessWidget {
   final String text;
   final Color backgroundcolor;
   final Color textColor;
+  final Icon icon;
   final Function() function;
   final double width;
   const CustomButton(
-      {super.key,
-      required this.function,
-      required this.width,
-      required this.text,
-      required this.backgroundcolor,
-      required this.textColor});
+    this.icon, {
+    super.key,
+    required this.function,
+    required this.width,
+    required this.text,
+    required this.backgroundcolor,
+    required this.textColor,
+  });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: function,
       borderRadius: BorderRadius.circular(15),
-      splashColor: Colors.white,
+      splashColor: backgroundcolor,
       child: Container(
         margin: const EdgeInsets.all(3),
         padding: const EdgeInsets.all(8),
         width: width,
-        height: 35,
+        // height: 35,
         decoration: BoxDecoration(
             color: backgroundcolor,
             borderRadius: BorderRadius.circular(8),
             border: Border.all(color: textColor, width: 1)),
         child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-                backgroundColor: backgroundcolor,
-                color: textColor,
-                fontWeight: FontWeight.bold),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              icon,
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                text,
+                style: TextStyle(
+                    backgroundColor: backgroundcolor,
+                    color: textColor,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
         ),
       ),
