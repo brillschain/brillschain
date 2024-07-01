238f6a94fcdee5fdf602f00586628c35e448fb23
naidu199 <141550407+naidu199@users.noreply.github.com>
2024-07-01 18:53:01 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,7 @@ class MyApp extends StatelessWidget {
               }
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(
-                  child: CircularProgressIndicator(
-                      // color: primaryColor,
-                      ),
+                  child: CircularProgressIndicator(),
                 );
               }
 
